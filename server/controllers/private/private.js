9fabe47c08d8cc2cfb0d13bd931fb02bdbd0879c
mohdafnaan <mohdafnaan833@gmal.com>
2025-11-28 21:17:19 +0530

    todov2

--- server/controllers/private/private.js
+++ server/controllers/private/private.js
@@ -1,6 +1,9 @@
 import express from "express";
 import { readDB,writeDB } from "../../utils/helper.js";
 import {v4 as uuid} from "uuid"
+import bcrypt from "bcrypt"
+import { OTPgenerator } from "../../utils/helper.js";
+import sendMail from "../../utils/sendEmail.js";
 
 const router = express.Router();
 
@@ -98,6 +101,129 @@ router.put("/updatetask/:id",async (req,res)=>{
         Object.assign(updatingTask,req.body);
         await writeDB(DB);
         res.status(200).json({msg : "task updated"})
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({msg : error})
+    }
+})
+// GET TASK BY ID
+router.get("/gettask/:id",async (req,res)=>{
+    try {
+        let DB = await readDB();
+        let existingUser = DB.find(u => u.id === req.user.id)
+        let Tid = req.params.id;
+        if(!Tid){
+            return res.status(404).json({msg : "Task not found"})
+        }
+        let task = existingUser.task.find(u => u.id === Tid);
+        await writeDB(DB);
+        res.status(200).json(task);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({msg : error})
+    }
+})
+// update user 
+router.put("/updateuser",async (req,res)=>{
+    try {
+        let DB= await readDB();
+        let existingUser = DB.find(u=>u.id === req.user.id);
+        if(!existingUser){
+            return res.status(404).json({msg :  "you are not an existing user"})
+        }
+        Object.assign(existingUser,req.body);
+        await writeDB(DB);
+        res.status(200).json({msg : "user details updated sucessfully"})
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({msg : error})
+    }
+})
+// delete existing user 
+router.delete("/delete",async (req,res)=>{
+    try {
+        let DB = await readDB();
+        let existingUser = DB.find(u=>u.id === req.user.id);
+        let remainingUsers = DB.filter(u=> u.email !== existingUser.email)
+        console.log(remainingUsers);
+        await writeDB(remainingUsers);
+        res.status(200).json({msg : "your account is deleted"})
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({msg : error})
+    }
+})
+// delete existing user with password
+router.delete("/deletebypass",async (req,res)=>{
+    try {
+        let DB = await readDB();
+        let existingUser = DB.find(u=>u.id === req.user.id);
+        let password = req.body.password;
+        let bpass = await bcrypt.compare(password,req.user.password);
+        if(!bpass){
+            return res.status(401).json({msg : "incorrect password"})
+        }
+        let allUsers = DB.filter(u=> u.id !== existingUser.id )
+        await writeDB(allUsers);
+        res.status(200).json({msg : "account deleted"})
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({msg : error})
+    }
+})
+// DELETE USER BY PASS AND GIVE OTP FOR VERIFICATION
+router.post("/otpwemail",async (req,res)=>{
+    try {
+        let DB = await readDB();
+        let existingUser = DB.find(u=>u.id === req.user.id);
+        let email = req.body.email;
+        if(email != existingUser.email){
+            return res.status(402).json({msg : "email is invalid"})
+        }
+        let password = req.body.password;
+        let bpass = await bcrypt.compare(password,existingUser.password);
+        if(!bpass){
+            return res.status(401).json({msg : "incorrect password"})
+        }
+        let otp = OTPgenerator();
+        existingUser.otp = otp;
+        await sendMail(email,"DELETATION OTP",`\n your OTP : ${otp}\n go to otp api and enter the OTP`)
+        await writeDB(DB)
+        res.status(200).json({msg : "mail sent"})
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({msg : error})
+    }
+})
+// delete user by otp
+router.post("/dltwotp",async(req,res)=>{
+    try {
+        let DB = await readDB();
+        let existingData = DB.find(u=>u.id === req.user.id);
+        if(!existingData){
+            return res.status(200).json({msg : "user not found"})
+        }
+        let OTP = req.body.otp;
+        if(OTP != existingData.otp){
+            console.log(req.user);
+            return res.status(404).json({msg : "incorrect OTP"})
+        }
+        let otherUsers = DB.filter(u=>u.id !== existingData.id);
+        await writeDB(otherUsers);
+        res.status(200).json({msg : "user Deleted sucessfully"})
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({msg : error})
+    }
+})
+// DELETE ALL USERS
+router.delete("/deleteallusers",async(req,res)=>{
+    try {
+        let DB = await readDB();
+        let allusers = DB.filter(u=> u !== u);
+        console.log(DB);
+        writeDB(allusers)
+        res.status(200).json({msg : "all users delete"})
     } catch (error) {
         console.log(error);
         res.status(500).json({msg : error})
--- server/tester.js
+++ server/tester.js
@@ -1,23 +1,34 @@
-import jwt from "jsonwebtoken";
-import dotenv from "dotenv";
-dotenv.config();
-let details = {
-    "fname" : "afnaan",
-    "age" : 20
-}
-async function encrypt(object){
-    let encryption = await jwt.sign(object,process.env.JWT_SECRET,{expiresIn:"1D"})
-    return encryption;
-}
-let code = await encrypt(details);
-console.log(code);
-//--------------------------------------------------------------------------
-// let a = "asdfgasdfg 2345674567";
-// console.log(a.split(" ")[1]);
+// import jwt from "jsonwebtoken";
+// import dotenv from "dotenv";
+// dotenv.config();
+// let details = {
+//     "fname" : "afnaan",
+//     "age" : 20
+// }
+// async function encrypt(object){
+//     let encryption = await jwt.sign(object,process.env.JWT_SECRET,{expiresIn:"1D"})
+//     return encryption;
+// }
+// let code = await encrypt(details);
+// console.log(code);
+// //--------------------------------------------------------------------------
+// // let a = "asdfgasdfg 2345674567";
+// // console.log(a.split(" ")[1]);
 
-async function decrypt(code)
-{
-    let decode = await jwt.verify(code, process.env.JWT_SECRET);
-    console.log(decode);
-}
-decrypt(code);
+// async function decrypt(code)
+// {
+//     let decode = await jwt.verify(code, process.env.JWT_SECRET);
+//     console.log(decode);
+// }
+// decrypt(code);
+
+
+
+let arr = [1,2,3,4,5];
+
+// let find = arr.find(x => x === 2 );
+// console.log(find);
+let DB = arr;
+// let filter = DB.filter(x=> x === x);
+
+
