6ebf1878ecbb43405b7280799a7ff42868bd5696
mohdafnaan <mohdafnaan833@gmal.com>
2025-11-30 01:11:03 +0530

    forgot password api

--- server/controllers/public/public.js
+++ server/controllers/public/public.js
@@ -5,6 +5,7 @@ import bcrypt from "bcrypt"
 import {v4 as uuid} from "uuid";
 import encrypt from "../../utils/token.js";
 import { registervalidation,loginValidation,otpValidation,errorvalidation } from "../../validators/valid.js";
+import Mail from "nodemailer/lib/mailer/index.js";
 
 
 const router = express.Router();
@@ -83,5 +84,64 @@ router.post("/login",loginValidation,errorvalidation,async (req,res)=>{
     }
 })
 
+// api for sending otp by taking email
 
+router.post("/emailforotp",async (req,res)=>{
+    try {
+        let DB = await readDB();
+        let email = req.body.email;
+        let user = DB.find(u=>u.email===email)
+        if(!user){
+            return res.status(401).json({msg : "user not found"})
+        }
+        let otp = OTPgenerator()
+        user.otp = otp;
+        await sendMail(email,"otp for resetting password", `your otp is ${otp}\n Enter this otp in http://localhost:5000/public/otpforpass`)
+        await writeDB(DB);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({msg : error})
+    }
+})
+// verify otp 
+router.post("/otpforpass",async(req,res)=>{
+    try {
+        let DB = await readDB();
+        let email = req.body.email;
+        let otp = req.body.otp;
+        let user = DB.find(u=>u.email===email)
+        if(!user){
+            return res.status(401).json({msg : "user not found"})
+        }
+        if(otp != user.otp){
+            return res.status(500).json({msg : "otp is invalid"})
+        }
+        delete user.otp;
+        await writeDB(DB);
+        res.status(200).json({msg : "go to update pass api and change password"})
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({msg : error})
+    }
+})
+
+router.put("/updatepass",async (req,res)=>{
+    try {
+        let DB = await readDB();
+        let email = req.body.email;
+        let password = req.body.password;
+        let hpass = await bcrypt.hash(password,10)
+        let user = DB.find(u=>u.email===email)
+        if(!user){
+            return res.status(401).json({msg : "user not found"})
+        }
+        user.password = hpass
+        await writeDB(DB);
+        res.status(200).json({msg : "password has been updated"})
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({msg : error})
+    }
+})
 export default router;
